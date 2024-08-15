767f70a50116efb2bf2fdb993b6218c6e0a7e8c9
Abrar Saad <abrarsaad3572022@gmail.com>
2024-08-15 17:46:42 +0300

    0. Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,50 @@
 #!/usr/bin/python3
+"""
+log parsing
+"""
+
 import sys
-import signal
+import re
 
-def print_statistics(total_size, status_counts):
-    print(f"File size: {total_size}")
-    for status_code in sorted(status_counts.keys()):
-        if status_counts[status_code] > 0:
-            print(f"{status_code}: {status_counts[status_code]}")
 
-def process_line(line, total_size, status_counts):
-    parts = line.split()
-    if len(parts) < 7:
-        return total_size
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-    try:
-        file_size = int(parts[-1])
-        status_code = int(parts[-2])
-    except (ValueError, IndexError):
-        return total_size
 
-    if status_code in status_counts:
-        status_counts[status_code] += 1
-    total_size += file_size
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
-    return total_size
-
-def main():
-    total_size = 0
-    status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     line_count = 0
-
-    def handle_interrupt(signal, frame):
-        print_statistics(total_size, status_counts)
-        sys.exit(0)
-
-    signal.signal(signal.SIGINT, handle_interrupt)
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
     try:
         for line in sys.stdin:
-            total_size = process_line(line.strip(), total_size, status_counts)
-            line_count += 1
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
-            if line_count % 10 == 0:
-                print_statistics(total_size, status_counts)
-    except KeyboardInterrupt:
-        print_statistics(total_size, status_counts)
-        sys.exit(0)
+                # File size
+                log["file_size"] += file_size
 
-    print_statistics(total_size, status_counts)
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
